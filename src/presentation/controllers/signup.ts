6aa0171eaeb5aeaf87f9c741725853c50b0cdc86
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-04-24 20:22:41 -0400

    feat: ensure SignUpController resturns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
-
 export class SigUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
