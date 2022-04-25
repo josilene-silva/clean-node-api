e33b3b48b608b18e84ea501ac8c2bb0dae73270b
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-04-24 20:17:09 -0400

    feat: ensure SignUpController returns an error if name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,8 @@
 export class SigUpController {
   handle (httpRequest: any): any {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Missing param: name')
     }
   }
 }
