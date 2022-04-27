df3d70a730b1a3beebb45727393376aecff069a0
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-04-26 20:14:17 -0400

    feat: ensure SignUpControntroller returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -31,11 +31,16 @@ export class SigUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
 
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
       })
+
+      return {
+        statusCode: 200,
+        body: account
+      }
     } catch (error) {
       return serverError()
     }
