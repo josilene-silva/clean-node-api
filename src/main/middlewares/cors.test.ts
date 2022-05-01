84865bb22af23bfb3f95dc1b2492f5597511f689
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-05-01 01:02:46 -0400

    fix: cors http method

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,7 +3,7 @@ import app from '../config/app'
 
 describe('CORS Middleware', () => {
   test('Should enable cors', async () => {
-    app.post('/test_cors', (req, res) => {
+    app.get('/test_cors', (req, res) => {
       res.send()
     })
     await request(app)
