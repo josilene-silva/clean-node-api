da2672e9b884ee7b6eee214c910c93faa918828a
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-05-13 00:21:31 -0400

    test: ensure LoginController returns 500 if Authentication throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -131,4 +131,15 @@ describe('Login Controller', () => {
 
     expect(authSpy).toHaveBeenCalledWith('any_mail@mail.com', 'any_password')
   })
+
+  test('Should returns 500 if Authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+
+    jest.spyOn(authenticationStub, 'auth')
+      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
