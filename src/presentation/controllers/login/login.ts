befc89b0dc3979ca1a289f1da1a309cb131f850e
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-05-13 00:25:35 -0400

    feat: ensure LoginController retruns 200 if valid credential are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
+import { badRequest, serverError, unauthorized, ok } from '../../helpers/http-helper'
 import { Authentication, EmailValidator, Controller, HttpRequest, HttpResponse } from './login-protocols'
 
 export class LoginController implements Controller {
@@ -30,6 +30,8 @@ export class LoginController implements Controller {
       if (!accessToken) {
         return unauthorized()
       }
+
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
