dd86a526078a20d3a8acdd6279aef113e3ce8b22
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-05-16 23:07:41 -0400

    feat: ensure DbAuthentication calls LoadAccountEmailRepository with correct email

---
+++ src/data/protocols/load-account-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../usecases/add-account/bd-add-account-protocols'
+
+export interface LoadAccountByEmailRepository {
+  load: (email: string) => Promise<AccountModel>
+}
