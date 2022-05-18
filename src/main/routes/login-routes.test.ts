e380c726886c1cec27bc909b53f2b911165843c5
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-05-18 18:48:04 -0400

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -53,5 +53,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'josilenevitoriasilva@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
