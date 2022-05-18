2c930508ec51e0dffe83315d7c00aff77da19880
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-05-18 18:07:47 -0400

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -16,18 +16,20 @@ describe('SignUp Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return an account on success', async () => {
-    app.post('/api/signup', (req, res) => {
-      res.send()
-    })
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Josilene',
-        email: 'josilenevitoriasilva@gmail.com',
-        password: '123',
-        passwordConfirmation: '123'
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      app.post('/api/signup', (req, res) => {
+        res.send()
       })
-      .expect(200)
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Josilene',
+          email: 'josilenevitoriasilva@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
   })
 })
